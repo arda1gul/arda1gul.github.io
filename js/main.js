1c16513ec3438948a91d32dcbe187f8db2d537fa
arda1gul <112638451+arda1gul@users.noreply.github.com>
2022-11-30 10:11:51 +0100

    changed layout and added player2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 console.log("Begin loaded")
 
-document.querySelector(".submit").onclick = function() {
-    let password = document.querySelector(".password").value;
-    if (password == 17) {
-        document.querySelector(".passcode").innerHTML = "Password unlocked!"
-        window.location.href = "scherm2.html"
-    } else {
-        document.querySelector(".passcode").innerHTML = "Password wrong!"
-    }
-}
+// document.querySelector(".submit").onclick = function() {
+//     let password = document.querySelector(".password").value;
+//     password = password.toUpperCase();
+//     if (password == "AMK") {
+//         document.querySelector(".passcode").innerHTML = "Password unlocked!"
+//         window.location.href = "scherm2.html"
+//     } else {
+//         document.querySelector(".passcode").innerHTML = "Password wrong!"
+//     }
+// }
 
 document.querySelector(".submitName").onclick = function() {
-    let myName = document.querySelector(".name").value;
-    document.querySelector(".naam").innerHTML = `Welkom ${myName}`
+    let playerOne = document.querySelector(".name").value;
+    document.querySelector(".naam").innerHTML = `Welkom ${playerOne}`
+}
+
+document.querySelector(".submit").onclick = function() {
+    let playerTwo = document.querySelector(".speler2").value;
+    document.querySelector(".naamTwee").innerHTML = `Welkom ${playerTwo}`
 }
