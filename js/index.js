95d07ed1be6b37dc46f56d1cca0bcd0a98457bbd
arda1gul <112638451+arda1gul@users.noreply.github.com>
2022-12-08 15:17:01 +0100

    made validations

---
+++ js/index.js
@@ -0,0 +1,37 @@
+console.log("JS Loaded")
+
+const playerScoreOne = 0;
+const playerScoreTwo = 0;
+let playerOne = ""
+let playerTwo = ""
+
+document.querySelector(".submitName").onclick = function () {
+    let playerOne = document.querySelector(".name").value;
+    if (playerOne == "")  {
+        document.querySelector(".naam").innerHTML = "Voer een naam in!"
+    } else {
+        document.querySelector(".naam").innerHTML = `Welkom ${playerOne}`
+    }
+    checkName()
+}
+
+document.querySelector(".submit").onclick = function () {
+    let playerTwo = document.querySelector(".speler2").value;
+    if (playerTwo == "")  {
+        document.querySelector(".naamTwee").innerHTML = "Voer een naam in!"
+    } else {
+        document.querySelector(".naamTwee").innerHTML = `Welkom ${playerTwo}`
+    }
+    checkName()
+}
+
+function checkName() {
+    if (playerOne == "" & playerTwo == "") {
+        console.log("Goedzo")
+    } else {
+        console.log("Loading...")
+        window.location.href = "scherm2.html"
+    }
+}
+
+
--- js/main.js
+++ js/main.js
@@ -1,29 +1,77 @@
 console.log("Begin loaded")
 
-// document.querySelector(".submit").onclick = function() {
-//     let password = document.querySelector(".password").value;
-//     password = password.toUpperCase();
-//     if (password == "AMK") {
-//         document.querySelector(".passcode").innerHTML = "Password unlocked!"
-//         window.location.href = "scherm2.html"
-//     } else {
-//         document.querySelector(".passcode").innerHTML = "Password wrong!"
-//     }
-// }
+let Button = document.querySelectorAll(".grid-item");
+let restartBtn = document.querySelector(".restart");
 
-const playerScoreOne = 0;
-const playerScoreTwo = 0;
+let winningPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [2, 5, 8],
+    [6, 7, 8],
+    [3, 4, 5],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 
-document.querySelector(".submitName").onclick = function() {
-    let playerOne = document.querySelector(".name").value;
-    document.querySelector(".naam").innerHTML = `Welkom ${playerOne}`
-}
+let xTurn = true;
+let count = 0;
 
-document.querySelector(".submit").onclick = function() {
-    let playerTwo = document.querySelector(".speler2").value;
-    document.querySelector(".naamTwee").innerHTML = `Welkom ${playerTwo}`
-    window.location.href = "scherm2.html"
-}
+const enableButtons = () => {
+    Button.forEach((element) => {
+        element.innerText = "";
+        element.disabled = false;
+    });
+};
 
-document.querySelector(".player1").innerHTML = `${playerOne} heeft ${playerScoreOne}`
-document.querySelector(".player2").innerHTML = `${playerTwo} heeft ${playerScoreTwo}`
+const winFunction = (letter) => {
+    if (letter == "X") {
+        alert('X heeft gewonnen');
+    } else {
+        alert('O heeft gewonnen');
+    }
+};
+
+const drawFunction = () => {
+    alert('It is a draw');
+};
+
+restartBtn.addEventListener("click", () => {
+    count = 0;
+    enableButtons();
+});
+
+const winChecker = () => {
+    for (let i of winningPattern) {
+        let [element1, element2, element3] = [
+            Button[i[0]].innerText,
+            Button[i[1]].innerText,
+            Button[i[2]].innerText,
+        ];
+        if (element1 != "" && (element2 != "") & (element3 != "")) {
+            if (element1 == element2 && element2 == element3) {
+                winFunction(element1);
+            }
+        }
+    }
+};
+
+Button.forEach((element) => {
+    element.addEventListener("click", () => {
+        if (xTurn) {
+            xTurn = false;
+            element.innerText = "X";
+            element.disabled = true;
+        } else {
+            xTurn = true;
+            element.innerText = "O";
+            element.disabled = true;
+        }
+        count += 1;
+        if (count == 9) {
+            drawFunction();
+        }
+        winChecker();
+    });
+});
+
