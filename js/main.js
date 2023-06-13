bf550a9908310bc4f475f73ac472353c8d47a016
arda1gul <112638451+arda1gul@users.noreply.github.com>
2023-06-13 15:42:55 +0200

    deleted includes in if statement

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const inputEmail = document.querySelector('.email');
 const inputMessage = document.querySelector('.message');
 const sendBtn = document.querySelector('.submit');
 sendBtn.addEventListener('click', function () {
-    if(inputName.value == "" && inputEmail.value.includes == "@" && inputMessage.value == "") {
+    if(inputName.value == "" && inputEmail.value == "" && inputMessage.value == "") {
         alert("Vul alles in!")
     } else {
         alert("Alles is ingevuld")
