90291319364ea3566a20ffea00d0c1da977cf4bd
arda1gul <112638451+arda1gul@users.noreply.github.com>
2023-06-14 23:57:09 +0200

    finished contact page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
 console.log("loaded");
-
-const inputName = document.querySelector('.name');
-const inputEmail = document.querySelector('.email');
-const inputMessage = document.querySelector('.message');
-const sendBtn = document.querySelector('.submit');
-sendBtn.addEventListener('click', function () {
-    if(inputName.value == "") {
-        alert("Vul uw naam in!")
-    }
-})
